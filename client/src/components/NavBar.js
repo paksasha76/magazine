b3f39ddf44f9ed2e1689627b26f1ec50927ede8f
paksasha76 <pak.aleksandr2001@mail.ru>
2026-01-28 12:46:34 +0300

    add http and devicepage

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,47 +1,50 @@
-import React from 'react';
-import { useContext } from 'react';
-import { observer } from 'mobx-react-lite';
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
-import { Context } from '..';
-import { NavLink } from 'react-router-dom';
-import { SHOP_ROUTE } from '../utils/consts';
+import React, {useContext} from 'react';
+import {Context} from "../index";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {NavLink, useNavigate} from "react-router-dom";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
+import {Button} from "react-bootstrap";
+import {observer} from "mobx-react-lite";
+import Container from "react-bootstrap/Container";
 
 const NavBar = observer(() => {
-    const { user } = useContext(Context);
-    console.log(user.isAuth);
+    const {user} = useContext(Context)
+    const navigate = useNavigate()
+
+    const logOut = () => {
+        user.setUser({})
+        user.setIsAuth(false)
+    }
+
     return (
-        <Navbar bg="dark" data-bs-theme="dark">
+        <Navbar bg="dark" variant="dark">
             <Container>
-            <NavLink style={{ color: 'white', textDecoration: 'none' }} to={SHOP_ROUTE}>
-                КупиДевайс
-            </NavLink>
-            {user.isAuth ? (
-                <Nav className="ms-auto" style={{ color: 'white' }}>
-                    <Button variant="outline-light">Админ панель</Button>
-                    <Button
-                        className='ms-2'
-                        variant="outline-light"
-                        onClick={() => {
-                            user.setIsAuth(false); // добавьте разлогин
-                        }}
-                    >
-                        Выйти
-                    </Button>
-                </Nav>
-            ) : (
-                <Nav className="ms-auto" style={{ color: 'white' }}>
-                    <Button
-                        variant="outline-light"
-                        onClick={() => {
-                            user.setIsAuth(true);
-                        }}
-                    >
-                        Авторизация
-                    </Button>
-                </Nav>
-            )}
+                <NavLink style={{color:'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
+                {user.isAuth ?
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => navigate(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => logOut()}
+                            className="ml-2"
+                        >
+                            Выйти
+                        </Button>
+                    </Nav>
+                    :
+                    <Nav className="ml-auto" style={{color: 'white'}}>
+                        <Button variant={"outline-light"} onClick={() => navigate(LOGIN_ROUTE)}>Авторизация</Button>
+                    </Nav>
+                }
             </Container>
         </Navbar>
+
     );
 });
 
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Card, Container, Form, FormControl, Row, Col } from 'react-bootstrap'
-import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts'
-import { NavLink, useLocation } from 'react-router-dom'
+import { LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from '../utils/consts'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { login, registration } from '../http/userAPI'
+import { Context } from '..'
 
 const Auth = () => {
+    const {user} = useContext(Context)
     const location = useLocation()
     const isLogin = location.pathname === LOGIN_ROUTE
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const click = async () => {
+        try {
+        let data;
+        if(isLogin) {
+            data = await login(email, password)
+        } else {
+            data = await registration(email, password)
+        }
+        user.setUser(user)
+        user.setIsAuth(true)
+        navigate(SHOP_ROUTE)
+    } 
+        catch(e) {
+        alert(e.response.data.message)
+        }
+    }
+
     return (
     <Container className='d-flex justify-content-center align-items-center'
     style={{height: window.innerHeight - 54}}
@@ -13,8 +37,8 @@ const Auth = () => {
         <Card style={{width: 600}} className='p-5'>
             <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
             <Form className="d-flex flex-column">
-                <FormControl className='mt-3' placeholder='Введите email'/>
-                 <FormControl className='mt-3' placeholder='Введите пароль'/>
+                <FormControl className='mt-3' placeholder='Введите email' value={email} onChange={e => setEmail(e.target.value)}/>
+                 <FormControl className='mt-3' placeholder='Введите пароль' value={password} onChange={e => setPassword(e.target.value)}/>
                 <Row className="mt-3">
         <Col className="d-flex justify-content-between align-items-center">
             {isLogin ? <div>
@@ -22,7 +46,7 @@ const Auth = () => {
             </div> :  <div>
                 Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите</NavLink>
             </div>}
-            <Button variant="outline-success">{isLogin ? 'Войти' : 'Регистрация'}</Button>
+            <Button variant="outline-success" onClick={click}>{isLogin ? 'Войти' : 'Регистрация'}</Button>
         </Col>
         </Row>
             </Form>
